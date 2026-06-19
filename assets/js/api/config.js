1ca3537890908ff697f1224773fbc2072466b89b
nesyasal <nesya.sr@gmail.com>
2026-06-19 23:42:57 +0700

    perbaikan url base api 2

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "https://be-elearning-production.up.railway.app/";
+export const API_BASE_URL = "https://be-elearning-production.up.railway.app";
 
 export async function apiFetch(endpoint, options = {}) {
   const token = localStorage.getItem("token");
